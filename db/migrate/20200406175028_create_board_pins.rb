3baadb2f9ffd99b445a478487842c64c209840a1
karlfleener <karlfleener@gmail.com>
2020-04-06 14:09:34 -0400

    first commit

---
+++ db/migrate/20200406175028_create_board_pins.rb
@@ -0,0 +1,10 @@
+class CreateBoardPins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :board_pins do |t|
+      t.integer :board_id, null: false
+      t.integer :pin_id, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200406175106_create_follows.rb
@@ -0,0 +1,11 @@
+class CreateFollows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :follows do |t|
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
+      t.string :followed_type, null: false
+
+      t.timestamps
+    end
+  end
+end
