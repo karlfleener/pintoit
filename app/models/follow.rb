8cefee7a628f9b2fc3b887f81da5989205e15d00
karlfleener <karlfleener@gmail.com>
2020-04-06 14:55:22 -0400

    user model/application controller auth

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id            :bigint           not null, primary key
+#  follower_id   :integer          not null
+#  followed_id   :integer          not null
+#  followed_type :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Follow < ApplicationRecord
 end
