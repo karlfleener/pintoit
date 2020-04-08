86da22be938b9a45b04509776db1e9b9640beb50
karlfleener <karlfleener@gmail.com>
2020-04-08 11:29:12 -0400

    signup form errors

--- frontend/components/session/signup_form_containter.js
+++ frontend/components/session/signup_form_containter.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions'
-import Signup from './signup_form';
+import SignupForm from './signup_form';
 
 const mapStateToProps = ({ errors }) => ({
   user: { email: '', password: '', age: ''},
-  errors: Object.values(errors.session)
-})
+  errors: Object.values(errors.session),
+});
 
 const mapDispatchToProps = dispatch => ({
   signup: user => dispatch(signup(user))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
