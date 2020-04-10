2dbbf553c38d0316d3a27692257261b6f8e9a9cc
karlfleener <karlfleener@gmail.com>
2020-04-10 19:48:03 -0400

    updated nav bar css

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -3,38 +3,30 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({ currentUser, logout }) => {
 
-  // const display = currentUser ? (
-  //   <div>
-  //     <p>{currentUser.email}</p>
-  //     <button onClick={logout}>Log out</button>
-  //   </div>
-  // ) : (
-  //   <div>
-  //     <Link className="btn" to="/signup">Sign up</Link>
-  //     <Link className="btn" to="/login">Log in</Link>
-  //   </div>
-  // )
-  
-
   return (
     <div className='nav-container'>
-      <Link to='/'><img src={window.logo} className="nav-logo" alt="Pintoit Logo"/></Link>
-    
-      <div className="search-bar">
-        <input 
-          className="search-bar-input"
-          type="text" 
-          placeholder="Search for Animal photography" 
-        />
-        <i className="fas fa-search" aria-hidden="true"></i>
-      </div>
 
-      <Link to="/"><div className="nav-item">Home</div></Link>
-      <Link to="/following"><div className="nav-item">Following</div></Link>
-      <Link to={`/${currentUser.email.slice(0, currentUser.email.indexOf('@'))}`}><div className="nav-item">{currentUser.email.slice(0, currentUser.email.indexOf('@'))}</div></Link> 
-      <div className='nav-item'><i className="fas fa-comment-dots"></i></div>      
-      <div className='nav-item'><i className="fas fa-bell"></i></div>  
-      <button className='nav-item logout-button' onClick={logout}>Log out</button>                            
+      <Link to='/'><img src={window.logo} className="nav-logo" alt="Pintoit Logo"/></Link>
+
+      <nav>
+
+        <Link to="/"><div className="nav-item home">Home</div></Link>
+        <Link to="/following"><div className="nav-item following">Following</div></Link>
+        <div className="search-bar">
+          <input 
+            className="search-bar-input"
+            type="text" 
+            placeholder="Search for Animal photography" 
+          />
+          <i className="fas fa-search" aria-hidden="true"></i>
+        </div>
+        <div className='nav-item notification'><i className="fas fa-bell"></i></div>  
+        <div className='nav-item inbox'><i className="fas fa-comment-dots"></i></div>      
+        <Link to={`/${currentUser.email.slice(0, currentUser.email.indexOf('@'))}`}><div className="nav-item profile">{currentUser.email.slice(0, currentUser.email.indexOf('@'))}</div></Link> 
+        <div><button className='nav-item logout-button' onClick={logout}>Log out</button></div>   
+      
+      </nav> 
+
     </div>
   )
 }
