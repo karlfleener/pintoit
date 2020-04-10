9c0579d9a8be40fe534c8fec360d7da6865dbe22
karlfleener <karlfleener@gmail.com>
2020-04-10 12:41:11 -0400

    testing routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :index, :show, :update, :destroy]
     resource :session, only: [:create, :destroy]
-    resources :boards, only: [:create, :index, :show, :edit, :update, :destroy]
-    resources :pins, only: [:create, :index, :show, :edit, :update, :destroy]
+    # resources :boards, only: [:create, :index, :show, :edit, :update, :destroy]
+    # resources :pins, only: [:create, :index, :show, :edit, :update, :destroy]
   end
 
   root "static_pages#root"
