4383a6100fc26ac3ea77df4b22db017fe5d96e77
karlfleener <karlfleener@gmail.com>
2020-04-15 17:24:12 -0400

    updated seed file with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,25 @@ user2 = User.create!({ id: 2, email: 'user2@gmail.com', password: 'password123',
 user2 = User.create!({ id: 3, email: 'user3@gmail.com', password: 'password123', age: 25})
 user_demo = User.create!({ id: 4, email: 'userdemo@gmail.com', password: 'password123', age: 25 })
 
-pin1 = Pin.create!({ id: 5, title: 'parrot', creator_id: 2})
-pin2 = Pin.create!({ id: 6, title: 'mountain creek', creator_id: 3})
-pin3 = Pin.create!({ id: 7, title: 'structure', creator_id: 1})
-pin4 = Pin.create!({ id: 8, title: 'demo pin', creator_id: 4})
-# pin7 = Pin.create({author_id: demouser.id, board_id: board2.id }) file = open('https://kim-seeds.s3.amazonaws.com/k7.jpg') pin7.photo.attach(io: file, filename: 'k7.jpg') pin7.save!
+pin1 = Pin.create!({ id: 5, title: 'snake', description: 'black snake' creator_id: 2})
+file = open('/app/assets/images/pins/snake.jpg')
+pin1.image.attach(io: file, filename: 'snake.jpg')
+pin1.save!
+
+pin2 = Pin.create!({ id: 6, title: 'desert sands', description: 'sahara desert', creator_id: 3})
+file = open('/app/assets/images/pins/desert.jpg')
+pin1.image.attach(io: file, filename: 'desert.jpg')
+pin1.save!
+
+pin3 = Pin.create!({ id: 7, title: 'gothic buttress', description: 'cathedral', creator_id: 1})
+file = open('/app/assets/images/pins/gothic_buttress.jpg')
+pin1.image.attach(io: file, filename: 'gothic_buttress.jpg')
+pin1.save!
+
+pin4 = Pin.create!({ id: 8, title: 'barcelona', description: 'the streets of barcelona', creator_id: 4})
+file = open('/app/assets/images/pins/barcelona.jpg')
+pin1.image.attach(io: file, filename: 'barcelona.jpg')
+pin1.save!
 
 board1 = Board.create!({ id: 9, title: 'animals', creator_id: 1})
 board2 = Board.create!({ id: 10, title: 'nature', creator_id: 2})
