c87e8fe8e97ee175bcd64610a7866a6513f2ca74
karlfleener <karlfleener@gmail.com>
2020-04-16 19:31:12 -0400

    profile header updated

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -14,34 +14,34 @@ class Profile extends React.Component {
     const { user } = this.props
     return (
       <div className='profile-container'>
-        <div className='profile-header'>
-          <div className="profile-edit-header">
-            <div className="create-pin-board-dropdown">
-              <button className='profile-create-pin-board-btn'><i className="fas fa-plus"></i></button>
-              <div className='profile-create-pin-board-content'>
-                <div>Create Board</div>
-                <Link to='/pin-builder'>Create Pin</Link>
-              </div>
+        <div className="profile-edit-header-bar"></div>
+        <div className='profile-create-header'>
+          <div className="profile-create-pin-board-dropdown">
+            <button className='profile-create-pin-board-btn'><i className="fas fa-plus"></i></button>
+            <div className='profile-create-hover'></div>
+            <div className='profile-create-pin-board-content'>
+              <div>Create Board</div>
+            <Link to='/pin-builder'><div>Create Pin</div></Link>
             </div>
           </div>
-          <div className='profile-info-header'>
-            <div>users email</div>
-            {/* <div>{user.email.slice(0, user.email.indexOf('@'))}</div> */}
-              <div className='profile-follow-container'>
-                <div>followers</div> 
-                <div>/</div>
-                <div>following</div>
-              </div>
-          </div>
-          <div className='profile-boards-pins-header'>
-            <button>Boards</button>
-            <button>Pins</button>
-          </div>
         </div>
-          <div>
-            <div>Users Board Index Container</div>
-            <div>Users Pin Index Container</div>
-          </div>
+        <div className='profile-info-header'>
+          <div className='profile-info-name'>users email</div>
+          {/* <div>{user.email.slice(0, user.email.indexOf('@'))}</div> */}
+            <div className='profile-follow-container'>
+              <div># followers</div> 
+              <div>/</div>
+              <div># following</div>
+            </div>
+        </div>
+        <div className='profile-boards-pins-header'>
+          <button>Boards</button>
+          <button>Pins</button>
+        </div>
+        <div className='profile-board-index-container'>
+          <div>Users Board Index Container</div>
+          <div>Users Pin Index Container</div>
+        </div>
       </div>
     )
   }
