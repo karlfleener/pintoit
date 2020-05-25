8cddb90022d9def4a684dd1b196aaac7071df442
karlfleener <karlfleener@gmail.com>
2020-05-25 18:03:04 -0400

    update config environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50.megabytes)
 end
