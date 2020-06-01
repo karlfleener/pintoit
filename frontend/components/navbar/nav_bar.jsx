a3a72b67c1086f9ecdfcb91282b4a7a2ab65cfad
karlfleener <karlfleener@gmail.com>
2020-06-01 14:35:42 -0400

    update nav css to grid

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -6,8 +6,8 @@ const NavBar = ({ currentUser, logout }) => {
   return (
     <div className='nav-container'>
       <nav className="nav-menu">
-        <div>
-          <Link to="/"><img src={window.logo} className="nav-logo" alt="Pintoit Logo" /></Link>
+        <div className="nav-logo">
+          <Link to="/"><img src={window.logo} alt="Pintoit Logo" className="logo" /></Link>
         </div>
         {/* <Link to="/following"><li className="nav-item following">Following</li></Link>
         <li className="search-bar">
@@ -19,15 +19,14 @@ const NavBar = ({ currentUser, logout }) => {
         <i className="fas fa-search" aria-hidden="true"></i>
         </li>
         <li className='nav-item notification'><i className="fas fa-bell"></i></li>  
-        <li className='nav-item inbox'><i className="fas fa-comment-dots"></i></li>      
-      <Link to={`/${currentUser.email.slice(0, currentUser.email.indexOf('@'))}`}> */}
-        <div>
-          <a href='https://www.linkedin.com/in/karlfleener/' target="_blank" className="nav-item"><i className="fab fa-linkedin about"></i></a>
-          <a href='https://angel.co/u/karlfleener' target="_blank" className="nav-item"><i className="fab fa-angellist about"></i></a>
-          <a href='https://github.com/karlfleener' target="_blank" className="nav-item"><i className="fab fa-github about"></i></a>
-          <a href='https://karlfleener.github.io/' target="_blank" className="nav-item"><i className="fas fa-user about"></i></a>
+        <li className='nav-item inbox'><i className="fas fa-comment-dots"></i></li> */}
+        <div className="about-links">
+          <a href='https://www.linkedin.com/in/karlfleener/' target="_blank" className="nav-item"><i className="fab fa-linkedin" id="about"></i></a>
+          <a href='https://angel.co/u/karlfleener' target="_blank" className="nav-item"><i className="fab fa-angellist" id="about"></i></a>
+          <a href='https://github.com/karlfleener' target="_blank" className="nav-item"><i className="fab fa-github" id="about"></i></a>
+          <a href='https://karlfleener.github.io/' target="_blank" className="nav-item"><i className="fas fa-user" id="about"></i></a>
         </div>
-        <div>
+        <div className="nav-links">
           <Link to="/"><div className="nav-item">Home</div></Link>
           <Link to={`/users/${currentUser.id}/boards`}><div className="nav-item">{currentUser.email.slice(0, currentUser.email.indexOf('@'))}</div></Link> 
           <div className='nav-item' onClick={logout}>Log out</div>
